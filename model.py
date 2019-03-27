b4c0f3eb09ec30c3f88b39e6320b457421658f25
StepanU <Stepanustinov@yandex.ru>
2019-03-27 20:11:27 +0300

    Update model.py

--- model.py
+++ model.py
@@ -3,16 +3,22 @@ from modeller.automodel import *
 
 
 def get_model(first_seq, second_seq):
+    """
+
+    :param first_seq:
+    :param second_seq:
+    :return:
+    """
     env = modeller.environ()
     aln = modeller.alignment(env)
 
     path = 'test_files/'
-    first_file = path + first_seq + '.ali'
+    first_file = path + first_seq + '.fasta'
     second_file = path + second_seq + '.pdb'
 
-    mdl = modeller.model(env, file=second_file)  # , model_segment=('FIRST:A', 'LAST:A'))
+    mdl = modeller.model(env, file=second_file)#, model_segment=('FIRST:L', 'LAST:H'))
     aln.append_model(mdl, align_codes=second_seq, atom_files=second_file)
-    aln.append(file=first_file, align_codes=first_seq)
+    aln.append(file=first_file, align_codes=first_seq, alignment_format='PIR')
     aln.align2d()
     aln.write(file='ans.ali', alignment_format='PIR')
 
@@ -36,4 +42,4 @@ def get_model(first_seq, second_seq):
         return None
 
 
-print(get_model("TvLDH", "1bdm"))
+print(get_model("1fgn", "1k6q"))
